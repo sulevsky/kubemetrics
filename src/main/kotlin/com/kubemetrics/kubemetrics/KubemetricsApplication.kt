92dd8f8bf0f82d26f07b8b6b051bacbbfc5ef578
vladimir.sulevskiy <vsu@conciliolabs.com>
2019-01-05 21:21:45 +0200

    Using environment variables

--- src/main/kotlin/com/kubemetrics/kubemetrics/KubemetricsApplication.kt
+++ src/main/kotlin/com/kubemetrics/kubemetrics/KubemetricsApplication.kt
@@ -1,10 +1,21 @@
 package com.kubemetrics.kubemetrics
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class KubemetricsApplication
+
+@RestController
+class HelloController(@Value("\${message}") val message: String) {
+    @GetMapping("/greeting")
+    fun getGreeting(): String {
+        return message
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<KubemetricsApplication>(*args)
