5267514d48433df80ab2d8a969faff3e567d5e9a
vladimir.sulevskiy <vsu@conciliolabs.com>
2019-01-05 19:14:21 +0200

    Spring app

---
+++ src/main/kotlin/com/kubemetrics/kubemetrics/KubemetricsApplication.kt
@@ -0,0 +1,12 @@
+package com.kubemetrics.kubemetrics
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KubemetricsApplication
+
+fun main(args: Array<String>) {
+    runApplication<KubemetricsApplication>(*args)
+}
+
